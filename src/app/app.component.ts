fabef2ea5698de8f1b8529c8c866962f82a095b6
hoaiphu99 <hoaiphu.ptit@gmail.com>
2022-02-11 17:51:20 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -6,15 +6,15 @@ import { AuthService } from './auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Expense Manager';
   isUserLoggedIn = false;
 
   constructor(private authService: AuthService) {}
 
-  NgOnInit() {
+  ngOnInit() {
     const storeData = localStorage.getItem('isUserLoggedIn');
-    console.log('Store Data:' + storeData);
+    console.log('Store Data: ' + storeData);
 
     if (storeData != null && storeData == 'true') {
       this.isUserLoggedIn = true;
